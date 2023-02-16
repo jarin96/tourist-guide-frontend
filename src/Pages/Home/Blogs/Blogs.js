0dc52d0be8cf9365dee163afbdfb327485427440
Jarin Nur A Anam <2017-1-60-027@std.ewubd.edu>
2023-02-16 15:42:20 +0600

    Blogs image uploaded

--- src/Pages/Home/Blogs/Blogs.js
+++ src/Pages/Home/Blogs/Blogs.js
@@ -1,21 +1,5 @@
 import React from "react";
 
-const blogs = [
-  {
-    id: 1,
-    title: "Nusa penida island, bali, indonesia",
-    description:
-      "Coxs Bazar is a city, fishing port, tourism centre, and district headquarters in Southeastern Bangladesh.",
-    img: "",
-  },
-  {
-    id: 2,
-    title: "Coxs Bazar",
-    description:
-      "Coxs Bazar is a city, fishing port, tourism centre, and district headquarters in Southeastern Bangladesh.",
-  },
-];
-
 const Blogs = () => {
   return (
     <div>
