2e7d97f43cbd8df47c352538dd9118ea8a9e6565
Jarin Nur A Anam <2017-1-60-027@std.ewubd.edu>
2023-02-16 14:51:17 +0600

    first commit

---
+++ src/Pages/Home/Blogs/Blogs.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const blogs = [
+  {
+    id: 1,
+    title: "Nusa penida island, bali, indonesia",
+    description:
+      "Coxs Bazar is a city, fishing port, tourism centre, and district headquarters in Southeastern Bangladesh.",
+    img: "",
+  },
+  {
+    id: 2,
+    title: "Coxs Bazar",
+    description:
+      "Coxs Bazar is a city, fishing port, tourism centre, and district headquarters in Southeastern Bangladesh.",
+  },
+];
+
+const Blogs = () => {
+  return (
+    <div>
+      <h2>Blogs</h2>
+    </div>
+  );
+};
+
+export default Blogs;
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <small> Copyright @2023</small>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
