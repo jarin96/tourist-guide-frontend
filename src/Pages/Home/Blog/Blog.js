f0b70c2ff268d09fecacd953f913361494afda0f
Jarin Nur A Anam <2017-1-60-027@std.ewubd.edu>
2023-02-16 19:44:59 +0600

    Google fonts added

--- src/Pages/Home/Blog/Blog.js
+++ src/Pages/Home/Blog/Blog.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./Blog.css";
 const Blog = ({ blog }) => {
   const { title, description, img } = blog;
   return (
     <div className="blog-description">
       <img className="blog-img" src={img} alt="" />
-      <h2> {title}</h2>
-      <p>
-        <small>{description}</small>
-      </p>
-      <button>Read More</button>
+      <div className="blog">
+        <h3> {title}</h3>
+        <p className="blog-des">
+          <small>{description}</small>
+        </p>
+        <Button variant="outline-dark">Read More</Button>
+      </div>
     </div>
   );
 };
