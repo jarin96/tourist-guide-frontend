6aafc8699782ef8725efc74d310c87c64ab7e05a
Jarin Nur A Anam <2017-1-60-027@std.ewubd.edu>
2023-02-17 15:46:18 +0600

    Footer Part done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,40 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>
-        <small> Copyright @2023</small>
-      </p>
+    <footer className="footer-content">
+      <div className="footer-text">
+        <h3>Tourist Guide BD</h3>
+        <p>
+          <p>
+            Tourist Guide Bangladesh (TGB) was formed in the year 2009 to meet
+            the demand of local and foreign tourist for guiding throughout
+            Bangladesh.
+          </p>
+          <small> Copyright @2023</small>
+
+          {/* <span class="fa-stack">
+            <i class="fa fa-circle-thin fa-stack-2x"></i>
+            <i class="fa fa-twitter fa-stack-1x"></i>
+          </span> */}
+
+          <ul className="socials">
+            <span class="fa-stack">
+              <i class="fa fa-circle-thin fa-stack-2x"></i>
+              <i class="fa fa-twitter fa-stack-1x"></i>
+            </span>
+            <span class="fa-stack">
+              <i class="fa fa-circle-thin fa-stack-2x"></i>
+              <i class="fa fa-facebook fa-stack-1x"></i>
+            </span>
+            <span class="fa-stack">
+              <i class="fa fa-circle-thin fa-stack-2x"></i>
+              <i class="fa fa-linkedin fa-stack-1x"></i>
+            </span>
+          </ul>
+        </p>
+      </div>
     </footer>
   );
 };
